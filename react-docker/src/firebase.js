5abaf63d1592ca99837c4d17b1a6741f79ff3269
GargAman10 <amangarg654321@gmail.com>
2024-04-23 16:32:41 +0530

    first commit

---
+++ react-docker/src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCSmnnCZHBQQC-NPi5ZxBdQ9c3fcI4poYY",
+  authDomain: "aman-project-83a99.firebaseapp.com",
+  projectId: "aman-project-83a99",
+  storageBucket: "aman-project-83a99.appspot.com",
+  messagingSenderId: "75884891683",
+  appId: "1:75884891683:web:0ed29e469f4d5b3d6c2b96"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
